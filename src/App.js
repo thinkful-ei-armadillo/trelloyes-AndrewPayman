963a5f59fd3cc5b7c5b3748b22134bf585a82e80
Payman Kossari <Paymankossari@gmail.com>
2019-02-04 14:42:33 -0500

    added card.js and basic initialization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function App() {
-  return <main className="App">{/* content goes here */}</main>;
+function App(props) {
+  return (
+    <main className="App">
+      <header class="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div class="App-list">
+        <section class="List">
+          <header class="List-header">
+            <h2>First list</h2>
+          </header>
+          <List />
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
